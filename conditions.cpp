dab0c4ec50d5b7c3f0690bb811dd998a970684e2
tsrebelo <rebelotelma01@gmail.com>
2023-10-18 12:35:13 +0000

    conditions.cpp

---
+++ conditions.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+
+int a = 10, b = 12, c = 9;
+
+int main(){
+
+    if (a < b ){                // "||" esse ou outro, "&&" e outro
+        cout << "é verdade" << endl;
+    }else if (a<c){
+        cout << "é verdade" << endl;
+        }else{
+            cout << "é mentira" << endl;
+    }
+
+
+
+    return 0;
+}
