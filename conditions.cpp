7d4cc63ad598b700a4fc98f98ee961ff23bd723c
tsrebelo <rebelotelma01@gmail.com>
2023-10-25 11:30:26 +0000

    conditions.cpp

--- conditions.cpp
+++ conditions.cpp
@@ -2,10 +2,9 @@
 using namespace std;
 
 
+/*
 int a = 10, b = 12, c = 9;
 
-
-/*
 int main(){
 
     if (a < b ){                                        // "||" esse ou outro, "&&" e outro
@@ -21,3 +20,27 @@ int main(){
     return 0;
 }
 */
+
+
+
+/**
+ * @brief peça um número e mostre se ele é positivo ou negativo
+ * 
+ * @return int
+*/
+
+
+int main(){
+    
+    int num;
+
+    cout << "Digite umn número: ";
+    cin >> num;
+
+    if(num >= 0){
+        cout << "é positivo" << endl;
+    }else if(num < 0){
+        cout << "é negativo" << endl;
+    }
+    return 0;
+}
