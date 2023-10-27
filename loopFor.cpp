1593d6e3406e121fab3bb7c8bf6de880fdaa943c
tsrebelo <rebelotelma01@gmail.com>
2023-10-27 09:18:44 +0000

    loopFor.cpp

--- loopFor.cpp
+++ loopFor.cpp
@@ -13,13 +13,19 @@ int main(){
 }
 */
 
+
 int main(){
     
 
     int i = 10;
 
-    while (i != 10){
+   // while (i != 10){                          //while faz eternamente
+   //     cout << "Olá" << endl;
+   // }
+
+    do {                                        //do while faz uma única vez, e depois avalia o código 
         cout << "Olá" << endl;
-    }
+    } while (i != 10);
+
     return 0;
 }
