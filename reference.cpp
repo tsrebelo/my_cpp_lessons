8a264f20f614ff1879d251000d80e72060da782e
tsrebelo <rebelotelma01@gmail.com>
2023-12-04 09:50:49 -0100

    reference.cpp

--- reference.cpp
+++ reference.cpp
@@ -11,9 +11,9 @@ string &meal = food;
 int main(){
 
     cout << food << "\n";                  //outPut "Pizza"
+    cout << &food << "\n";                 //outPut endereço de memória 0x55834a084160
     cout << meal << "\n";                  //outPut "Pizza"
-    cout << &food << "\n";
-   
+    cout << &meal << "\n";                 //outPut endereço de memória 0x55834a084160
    
     return 0;
 }
