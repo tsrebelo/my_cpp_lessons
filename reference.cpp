12d2386043bbe6400db11e3ed72bbfedb72f1837
tsrebelo <rebelotelma01@gmail.com>
2023-12-04 09:34:46 -0100

    reference.cpp

---
+++ reference.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+//reference is an alias for a variable
+string food = "Pizza";
+
+//create a reference variable
+string &meal = food;
+
+
+int main(){
+
+    
+}
