169f9c556f4874f3e34ff180cfbd3970a537d8f8
Telma Rebelo <rebelotelma01@gmail.com>
2024-02-06 09:52:37 -0100

    my cpp lessons

--- 2mod/fncPrintf.cpp
+++ 2mod/fncPrintf.cpp
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
+/*
 int main(){
     printf("teste\n");
     printf("Characters: %c %c %c \n", 'e', 'i', 'o');
+    printf("Decimals: %d %ld \n", 1977, 650000L);
+    printf("Preceding with blanks: %10d \n", 1932477);
+    printf("Width trick: %*d \n", 10, 10);
+}
+*/
+
+int main(){
+
+    char nome[50];
+
+    system("clear");
+
+    printf("Nome: ");
+    scanf("%s", nome);
+    printf("Olá %s\n", nome);
 }
