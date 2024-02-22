141ade416c10c0ec42bd47e0a53b6a53d88e3abb
Telma Rebelo <rebelotelma01@gmail.com>
2024-02-22 10:34:56 -0100

    updates

--- 2mod/switchCaseEx1.cpp
+++ 2mod/switchCaseEx1.cpp
@@ -40,6 +40,23 @@ void executaCalculo(int n){
     switch(i){
         case 1:
         
+            break;
+
+        case 2:
+
+            break;
+
+        case 3:
+
+            break;
+
+        case 4:
+
+            break;
+
+        default:
+
+            break;
 
     }
 }
