8ff636dbdbf0a621db7520b8d27a8059b3242d15
Telma Rebelo <rebelotelma01@gmail.com>
2024-02-22 09:18:09 -0100

    updates

---
+++ 2mod/swtchCase.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+    switch (expression)
+    {
+    case /* constant-expression */:
+        /* code */
+        break;
+    
+    default:
+        break;
+    }
+
+    return 0;
+}
