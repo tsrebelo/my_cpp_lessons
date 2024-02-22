4a8fcc426f95809cac23f570d9c0a6d89c829d8c
Telma Rebelo <rebelotelma01@gmail.com>
2024-02-22 09:36:28 -0100

    updates

--- 2mod/swtchCase.cpp
+++ 2mod/swtchCase.cpp
@@ -9,9 +9,15 @@ int main(){
         case 1:
             cout << " n é 1 " << endl;
         break;
+
         case 2:
             cout << "n é 2" << endl;
         break;
+
+        case 3:
+            cout << "n é 3" << endl;
+        break;
+        
         default:
             break;
     }
