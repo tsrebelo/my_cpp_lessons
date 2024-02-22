f85bba874c884769cb1983f6c94f8b7a9f541762
Telma Rebelo <rebelotelma01@gmail.com>
2024-02-22 10:14:58 -0100

    updates

--- 2mod/switchCaseEx1.cpp
+++ 2mod/switchCaseEx1.cpp
@@ -11,7 +11,7 @@ void mult(int a, int b){
     cout << a * b << endl;
 }
 
-void div(int a, int b){
+void divi(int a, int b){
     cout << a / b << endl;
 }
 
@@ -20,9 +20,14 @@ void restDiv(int a, int b){
 }
 
 void menu(){
-    cout << "==================" << endl;
-    cout << "       MENU" << endl;
-    cout << "==================" << endl;
+    cout << "======================" << endl;
+    cout << "         MENU" << endl;
+    cout << "======================" << endl;
+    cout << "1 - Soma " << endl;
+    cout << "2 - Multiplicação " << endl;
+    cout << "3 - Divisão " << endl;
+    cout << "4 - Resto da divisão " << endl;
+    cout << "======================" << endl;
 }
 
 int main(){
