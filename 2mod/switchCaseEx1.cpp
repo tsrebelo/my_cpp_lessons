a7026bf4673075d4cb2940b16332387f8ae458ab
Telma Rebelo <rebelotelma01@gmail.com>
2024-02-22 10:11:03 -0100

    updates

---
+++ 2mod/switchCaseEx1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+void soma(int a, int b){
+    cout << a + b << endl;
+}
+
+void mult(int a, int b){
+    cout << a * b << endl;
+}
+
+void div(int a, int b){
+    cout << a / b << endl;
+}
+
+void restDiv(int a, int b){
+    cout << a % b << endl;
+}
+
+void menu(){
+    cout << "==================" << endl;
+    cout << "       MENU" << endl;
+    cout << "==================" << endl;
+}
+
+int main(){
+
+
+    return 0;
+}
