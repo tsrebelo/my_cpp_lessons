f8dc079c863385e21c38564401dfde51c7d41b3f
Telma Rebelo <rebelotelma01@gmail.com>
2024-02-27 09:25:18 -0100

    updates

--- 2mod/controlo.cpp
+++ 2mod/controlo.cpp
@@ -3,11 +3,14 @@ using namespace std;
 
 int main(){
 
-    int num, sum;
+    int num, sum = 0;
 
     for(num = 1; num <= 10; num++){
         if(num%4==0)
             continue;
         sum += num;
     }
+
+    cout << "Total: " << sum << endl;
+    return 0;
 }
