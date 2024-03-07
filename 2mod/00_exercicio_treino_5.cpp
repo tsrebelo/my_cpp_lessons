cf955f5e31a299d96ceebd66d9bb9bd4ddeddab0
Telma Rebelo <rebelotelma01@gmail.com>
2024-03-07 10:10:54 -0100

    updates

---
+++ 2mod/00_exercicio_treino_5.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int arr[10] = {1,2,3,4,5,6,7,8,9,10};
+
+int somaArr(){
+    int sum = 0;
+    for(int x=0; x<=10; x++){
+        sum+=arr[x];
+    
+    }
+    return sum;
+}
+
+int main(){
+    cout << "a soma dos elementos é: " << somaArr() << endl;
+}
