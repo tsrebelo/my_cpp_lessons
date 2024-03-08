ec866ae1c99ca05bca35adcc27cda61dbb3496cf
Telma Rebelo <rebelotelma01@gmail.com>
2024-03-08 10:05:22 -0100

    updates

---
+++ exercicios/exercicio_while.cpp
@@ -0,0 +1,22 @@
+/**
+ * @brief Faz um exercicio onde faças o mesmo que o loopFor só que em whileDo
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    
+    for(int n=0; n<=10; n++){                                   
+        cout << n << endl;
+    }
+cout << "_________________________________\n" << endl;
+
+    int x;
+    while(x<=10){
+        cout << x << endl;
+        x++;
+    }
+
+    return 0;
+}
