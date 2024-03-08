8a073e90d17f4056e60faac81cd46140de899cfc
Telma Rebelo <rebelotelma01@gmail.com>
2024-03-08 09:43:10 -0100

    updates

---
+++ 2mod/scope.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+const float PI = 3.1415;
+float n = 2.3;
+
+int teste(){
+    const int xpto = 3;
+    
+    return xpto;
+}
+
+int main(){
+
+    system("clear");
+    
+    cout << teste() << endl;
+
+}
