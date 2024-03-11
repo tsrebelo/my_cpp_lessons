5b0a7a2aea708ee0d2d94adc8aa801771c97cbb4
Telma Rebelo <rebelotelma01@gmail.com>
2024-03-11 09:56:54 -0100

    updates

--- 2mod/namespace.cpp
+++ 2mod/namespace.cpp
@@ -1,12 +1,15 @@
 #include <iostream>
 using namespace std;
 
+int n = 10;
 
 namespace xpto{
+    int x = 20;
     void func(){
         cout << "xpto1" << endl;
     }
     namespace xpto2{
+        int x = 30;
         void func(){
             cout << "xpto2" << endl;
         }
@@ -14,6 +17,7 @@ namespace xpto{
 }
 
 namespace first_space{
+    int x = 40;
     void func1(){
         cout << "Inside first1_space" << endl;
     }
@@ -23,8 +27,22 @@ namespace first_space{
 }
 
 namespace second_space{
+    int x = 50;
         void func(){
         cout << "Inside second_space" << endl;
+    }
+}
+
+namespace a{
+    int x = 40;
+    void func(){
+        cout << "a" << endl;
+    }
+}
+
+namespace b{
+        void func(){
+        cout << a::x << endl;                       //imprime o valor de x do namespace a
     }
 }
 
@@ -35,10 +53,21 @@ int main(){
 
     system("clear");
 
+    cout << n << "\n";
+
+    b::func();                                  
+
+    cout << xpto::x << "\n";
+    cout << xpto::xpto2::x << "\n";
+
     func();
     xpto::xpto2::func();
+
     first_space::func2();
     first_space::func2();
+    
     second_space::func();
+
+
     return 0;
 }
