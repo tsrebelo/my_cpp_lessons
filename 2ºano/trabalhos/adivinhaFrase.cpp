3a1e72edff9e29fb255caf6d5e4a42439ce6f2b6
tsrebelo <rebelotelma01@gmail.com>
2025-02-07 12:11:12 -0100

    updates

--- 2ºano/trabalhos/adivinhaFrase.cpp
+++ 2ºano/trabalhos/adivinhaFrase.cpp
@@ -1,26 +1,74 @@
+/**
+ * @author Telma Silva Rebelo e Martim Valério
+ * @date 07-02-2025
+ */
+
 #include <iostream>
 using namespace std;
 
-void frase(){
-
-    string frase;
-    string entrada;
-    bool acertou = false;
-
-    cout << "Insira uma frase:";
-    getline(cin.ignore(), frase);
-
-    string linhas(frase.size(), '_');
-
-
-}
-
 int main(){
+    system("clear");
+    string frase, adivinha;
+    string entrada;
+    char letra;
+
+    cout << "Insira uma frase: ";
+    getline(cin, frase);
+
+    for(char n : frase){
+        if(n != ' '){
+            adivinha += '_'; 
+        }else{
+            adivinha += ' '; 
+        }
+    }
 
     system("clear");
-    cout << "Bem-Vindo/a ao jogo do Adivinha a Frase!\n" << endl;
 
-    frase();
+    cout << "Adivinhe a frase!" << endl;
+
+    while(adivinha != frase){
+
+
+        cout << "Frase a ser adivinhada: " << adivinha << endl;
+        cout << "Digite uma letra ou a frase inteira: ";
+        getline(cin, entrada);
+        bool entradaCorreta = false;
+
+        if(entrada.size() == 1){
+            letra = entrada[0];
+            entradaCorreta = false;
+
+            for(int i = 0; i < frase.size(); i++){
+                if (frase[i] == letra){
+                    adivinha[i] = letra;
+                    entradaCorreta = true;
+                }
+            }
+
+            if(!entradaCorreta){
+                system("clear");
+                cout << "Errou a letra!\n" << endl;
+
+            } else{
+                system("clear");
+                cout << "Acertou a letra!\n" << endl;
+            } 
+        }
+
+        else {
+            if (entrada == frase) {
+                adivinha = frase; 
+                break;
+            } else {
+                system("clear");
+                cout << "Frase incorreta! \n" << endl;
+            }
+        }
+    }
+    system("clear");
+    cout << "Parabéns acertou a frase: " << frase << endl;
+
 
     return 0;
- }
+}
