c7db9be57c30f169c09712e94b1056e9dd4877fa
telma <145616594+tsrebelo@users.noreply.github.com>
2025-03-24 10:30:10 -0100

    Update bibliotecas.h

--- BINGO/test/bibliotecas.h
+++ BINGO/test/bibliotecas.h
@@ -12,16 +12,4 @@
 
 using namespace std;
 
-// Códigos ANSI para cores e formatação
-#define RESET   "\033[0m"
-#define BOLD    "\033[1m"
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define BLUE    "\033[34m"
-#define MAGENTA "\033[35m"
-#define CYAN    "\033[36m"
-#define WHITE   "\033[37m"
-#define BG_PINK "\033[48;5;205m"
-
 #endif
