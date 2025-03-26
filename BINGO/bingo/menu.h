55dcf034c40f0a8758e8a2987d45b6aeb555e1f0
tsrebelo <rebelotelma01@gmail.com>
2025-03-26 16:30:19 -0100

    updates

--- BINGO/bingo/menu.h
+++ BINGO/bingo/menu.h
@@ -8,8 +8,22 @@
 #ifndef MENU_H
 #define MENU_H
 
+/**
+ * @brief Função que exibe o menu principal do jogo
+ * 
+ */
 void exibirMenuPrincipal();
+
+/**
+ * @brief Função que exibe o menu para criação de cartas
+ * 
+ */
 void exibirMenuCriarCartas();
+
+/**
+ * @brief função que exibe o menu de seleção de jogo
+ * 
+ */
 void exibirMenuJogo();
 
 #endif
