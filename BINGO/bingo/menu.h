76aeaed7d17d0e2a2f6fab9d810e452d058f31d8
tsrebelo <rebelotelma01@gmail.com>
2025-03-26 16:30:32 -0100

    updates

--- BINGO/bingo/menu.h
+++ BINGO/bingo/menu.h
@@ -1,7 +1,5 @@
 /**
  * @brief Ficheiro de declaração das funções de exibição de menus
- * Respetivamente, função que exibe o menu principal do jogo, função que exibe o menu
- * Para criação de cartas e função que exibe o menu de seleção de jogo
  * 
  */
 
