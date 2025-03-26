01243b5309439ef69c83b2a688d4122d0163189c
tsrebelo <rebelotelma01@gmail.com>
2025-03-26 16:59:52 -0100

    updates

--- BINGO/bingo/main.cpp
+++ BINGO/bingo/main.cpp
@@ -3,7 +3,7 @@
  * @date 26/03/2025
  * 
  * @brief Ficheiro principal que inicia a execução do jogo do bingo.
- * Contém apenas a função main que chama a função executarJogo().
+ * Contém apenas a função main que invoca a função executarJogo().
  * 
  */
 
